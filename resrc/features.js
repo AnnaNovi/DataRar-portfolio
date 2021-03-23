6e40d5e1b5a2316fb4bc24dcb644f7cf6da238d9
AnnaNovi <77693820+AnnaNovi@users.noreply.github.com>
2021-03-23 16:59:19 +0300

    Add files via upload

---
+++ resrc/features.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.vacancy-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        centerMode: true,
+        centerPadding: '120px',
+        variableWidth: true
+    });
+});
