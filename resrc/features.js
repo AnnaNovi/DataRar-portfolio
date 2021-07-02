48122a086fa03667a8df2ee74774f1625a1b54b5
AnnaNovi <77693820+AnnaNovi@users.noreply.github.com>
2021-07-02 19:08:08 +0300

    Add files via upload

--- resrc/features.js
+++ resrc/features.js
@@ -1,24 +1,58 @@
 $(document).ready(function(){
-    $('.vacancy-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 4,
-        centerMode: true,
-        centerPadding: '120px',
-        variableWidth: true
-    });
+    if (window.innerWidth <= 479){
+        $('.vacancy-slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 4,
+            centerMode: true,
+            centerPadding: '0px',
+            variableWidth: true
+        });
+    }
+    else if (window.innerWidth >= 500){
+        $('.vacancy-slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 4,
+            centerMode: true,
+            centerPadding: '120px',
+            variableWidth: true
+        });
+    }
 });
 
 
 $(document).ready(function(){
-    $('.reviews-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 2,
-        centerMode: true,
-        centerPadding: '245px',
-        variableWidth: true
-    });
+    if (window.innerWidth <= 992){
+        $('.reviews-slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            centerMode: true,
+            centerPadding: '0px',
+            variableWidth: true
+        });
+    }
+    else if (window.innerWidth >= 992){
+        $('.reviews-slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 2,
+            centerMode: true,
+            centerPadding: '245px',
+            variableWidth: true
+        });
+    }
 });
+
+$(document).ready(function(){
+	$('.header__burger').click(function(event){
+		$('.header__burger, .header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
